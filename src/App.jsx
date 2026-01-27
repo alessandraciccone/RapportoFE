9833f5e3abdf3445bd56d2d474b10e0f12267d0a
alessandraciccone <alessandraciccone@icloud.com>
2026-01-27 21:05:15 +0100

    install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-red-400 font-mono text-4xl text-gray-200 m-4 text-center">
+        Hello World
+      </h2>
+    </>
+  );
+}
+
+export default App;
